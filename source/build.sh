627c298e4f3d1ed5fe0e6224db0ca5069e52ceed
gentauro <gentauro@gmail.com>
2009-05-10 11:53:02 +0200

    forgot to make executables for joakims tests ... we should not add let _ in our libraries but make separate test files

--- source/build.sh
+++ source/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 ocamlc -vmthread threads.cma -c csp.ml
 ocamlc -c legoland.ml
+ocamlc -vmthread threads.cma -o csp csp.cmo
+ocamlc -vmthread threads.cma -o legoland csp.cmo legoland.cmo

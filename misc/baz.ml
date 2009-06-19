0c41ef6ff6f3de1998546eafe80cb7ab527103c2
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2009-06-19 11:56:10 +0200

    The new strategy seems to work now after a minor bug fix.

--- misc/baz.ml
+++ misc/baz.ml
@@ -18,16 +18,20 @@ type 'a guard = {
 
 let channel () = ref NobodyWaiting
 
+(* Must be called in a locked context *)
 let rec attempt l = match l with
     | [] -> None
     | (h::t) -> match h.attempt () with
         | Some v -> Some v
         | None -> attempt t
 
-let rec subscribe l r = match l with
+(* Must be called in a locked context *)
+let subscribe l r = let rec loop k = match k with
     | [] -> ()
-    | (h::t) -> h.subscribe l r; subscribe t r
+    | (h::t) -> h.subscribe l r; loop t
+    in loop l
 
+(* Uses the global lock *)
 let select l =
     Mutex.lock global_mutex;
     match attempt l with
@@ -38,14 +42,15 @@ let select l =
         | None -> (Condition.wait global_condition global_mutex; loop ())
         in loop ())
 
-let read_guard c = {
+(* Methods must be called in a locked context *)
+let read_guard c f = {
     attempt = (fun () -> match !c with
-        | WriterWaiting x -> Some (x ())
+        | WriterWaiting x -> Some (f (x ()))
         | _ -> None
     );
     subscribe = (fun l r -> match !c with
         | NobodyWaiting -> c := ReaderWaiting (fun v ->
-            r := Some v;
+            r := Some (f v);
             (let rec loop l = match l with
             | [] -> ()
             | (h::t) -> h.unsubscribe (); loop t
@@ -56,14 +61,15 @@ let read_guard c = {
     unsubscribe = (fun () -> c := NobodyWaiting);
     }
 
-let write_guard c v = {
+(* Methods must be called in a locked context *)
+let write_guard c v f = {
     attempt = (fun () -> match !c with
-        | ReaderWaiting x -> (x v; Some ())
+        | ReaderWaiting x -> (x v; Some (f v))
         | _ -> None
     );
     subscribe = (fun l r -> match !c with
         | NobodyWaiting -> c := WriterWaiting (fun () ->
-            r := Some ();
+            r := Some (f v);
             (let rec loop l = match l with
             | [] -> ()
             | (h::t) -> h.unsubscribe (); loop t
@@ -75,23 +81,49 @@ let write_guard c v = {
     unsubscribe = (fun () -> c := NobodyWaiting);
     }
 
-let read c = select [read_guard c]
-let write c v = select [write_guard c v]
+
+let read c = select [read_guard c (fun x -> x)]
+let write c v = select [write_guard c v (fun _ -> ())]
 
 let parallel fs =
     let ts = List.map (fun f -> Thread.create f ()) fs in
     List.iter Thread.join ts
 
+(*
 let _ = 
     let c1 = channel () in
     let c2 = channel () in
+    let c3 = channel () in
     parallel [
     (let rec loop () = let v = select [
         read_guard c1;
         read_guard c2;
     ] in print_endline v; loop () in loop);
-    (let rec loop () = write c1 "1"; loop () in loop);
+    (let rec loop () = print_endline (read c3); loop () in loop);
     (let rec loop () = write c2 "2"; loop () in loop);
+    (let rec loop () = select [write_guard c1 "1"; write_guard c3 "3"]; loop () in loop);
     ];
-    
+*)
+
+let id x = x
+
+let _ = 
+    let c1 = channel () in
+    let c2 = channel () in
+    let c3 = channel () in
+    parallel [
+
+        (let rec loop () = let v = select [
+            read_guard c1 id;
+            read_guard c2 id;
+            write_guard c3 "3" (fun _ -> "write 3");
+        ] in print_endline v; loop () in loop);
+
+        (let rec loop () = let v = select [
+            read_guard c3 id;
+            write_guard c2 "2" (fun _ -> "write 2");
+            write_guard c1 "1" (fun _ -> "write 1");
+        ] in print_endline v; loop () in loop);
+
+    ];
 

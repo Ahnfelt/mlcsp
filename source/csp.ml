8e58d80289b6f53d585f91fe584c0ff9622e5195
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2009-06-24 12:59:21 +0200

    Restructuring

--- source/csp.ml
+++ source/csp.ml
@@ -1,205 +1,194 @@
-(*******************************************************************************)
-(*                                                                             *)
-(*                       Objective Caml CSP Library                            *)
-(*                                                                             *)
-(*Copyleft (l) 2009 Joakim Ahnfelt-Rønne and Ramón Salvador Soto Mathiesen     *)
-(*                                                                             *)
-(*Permission is hereby granted, free of charge, to any person obtaining a copy *)
-(*of this software and associated documentation files (the "Software"), to deal*)
-(*in the Software without restriction, including without limitation the rights *)
-(*to use, copy, modify, merge, publish, distribute, sublicense, and/or sell    *)
-(*copies of the Software, and to permit persons to whom the Software is        *)
-(*furnished to do so, subject to the following conditions:                     *)
-(*                                                                             *)
-(*The above copyright notice and this permission notice shall be included in   *)
-(*all copies or substantial portions of the Software.                          *)
-(*                                                                             *)
-(*THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   *)
-(*IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     *)
-(*FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  *)
-(*AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       *)
-(*LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,*)
-(*OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN    *)
-(*THE SOFTWARE.                                                                *)
-(*                                                                             *)
-(*******************************************************************************)
+(* Compile with: ocamlc -vmthread threads.cma -o csp csp.mli csp.ml *)
 
-(* $Id: csp.ml,v 1.0 2009/05/05 09:00:00 gentauro Exp $ *)
+(* Argumenter for poison *)
+(* Hvad sker der hvis alternativ to er forgiftet, 
+   men alternativ et er klar? Lige nu læser den vidst
+   fra den klare, men det er måske ikke 
+   hensigtsmæssigt. Det er også muligt at select kun
+   skal kaste PoisonException hvis alle dens 
+   valgmuligheder er poisoned. *)
+(* Poison er ændret til den alternative opførsel. *)
+(* Burde poison propagere til andre kanaler?
+   Det er nok ikke hensigtsmæssigt, idet der alligevel
+   skal gøres noget specielt når der er flere kanaler
+   i en process end der er i den nuværende select.
+   Alternativt kan processen "huske" alle kanaler den
+   har oprettet eller fået fingre i - dette er dog 
+   næppe muligt i det generelle tilfælde. *)
+(* Overvej conditional guards. *)
+(* Overvej timeout guards *)
 
+(* Consider making a toolbox that has parallel_collect and derivatives *)
 
-module Csp = struct
+exception PoisonException
 
+type 'a concrete_guard = {
+    attempt: unit -> 'a option;
+    check_poison: unit -> bool;
+    subscribe: ('a concrete_guard) list -> ('a option) ref -> unit;
+    unsubscribe: unit -> unit;
+    }
 
-    module type CHANNEL = sig
+type 'a guard = Condition.t -> 'a concrete_guard
 
-        type ('a, 'c) t
-        
-        exception PoisonException
-        
-        val channel : unit -> ('a, [`Read | `Write | `Poison]) t
-        
-        val read : ('a, [> `Read]) t -> 'a
-        val write : ('a, [> `Write]) t -> 'a -> unit
-        val poison : ('a, [> `Poison]) t -> unit
+type 'a channel_state
+    = NobodyWaiting 
+    | ReaderWaiting of (Condition.t * ('a -> unit)) list
+    | WriterWaiting of (Condition.t * (unit -> 'a)) list
+    | Poisoned
 
-        val poison_only : ('a, [> `Poison]) t -> ('a, [`Poison]) t
-        val write_only : ('a, [> `Write]) t -> ('a, [`Write]) t
-        val write_poison_only : ('a, [> `Write | `Poison]) t -> ('a, [`Write | `Poison]) t
-        val read_only : ('a, [> `Read]) t -> ('a, [`Read]) t
-        val read_poison_only : ('a, [> `Read | `Poison]) t -> ('a, [`Read | `Poison]) t
-        val read_write_only : ('a, [> `Read | `Write]) t -> ('a, [`Read | `Write]) t
-        
-    end
+type ('a, 'b) channel = ('a channel_state) ref
 
+type on = unit and off = unit
 
-    module type PROCESS = sig
+type 'a t = ('a, on * on * on) channel
 
-        val spawn : int -> (int -> unit) -> unit
+type ('a, 'b) either = Left of 'a | Right of 'b
 
-        val fork : (unit -> unit) list -> unit
+let global_mutex = Mutex.create ()
 
-        val parallel : (unit -> 'a) -> (unit -> 'b) -> 'a * 'b
-        
-        val parallel3 : (unit -> 'a) -> (unit -> 'b) -> (unit -> 'c) -> 'a * 'b * 'c
-        val parallel4 : (unit -> 'a) -> (unit -> 'b) -> (unit -> 'c) -> (unit -> 'd) -> 'a * 'b * 'c * 'd
-        val parallel5 : (unit -> 'a) -> (unit -> 'b) -> (unit -> 'c) -> (unit -> 'd) -> (unit -> 'e) -> 'a * 'b * 'c * 'd * 'e
-        val parallel6 : (unit -> 'a) -> (unit -> 'b) -> (unit -> 'c) -> (unit -> 'd) -> (unit -> 'e) -> (unit -> 'f) -> 'a * 'b * 'c * 'd * 'e * 'f
-        val parallel7 : (unit -> 'a) -> (unit -> 'b) -> (unit -> 'c) -> (unit -> 'd) -> (unit -> 'e) -> (unit -> 'f) -> (unit -> 'g) -> 'a * 'b * 'c * 'd * 'e * 'f * 'g
-        val parallel8 : (unit -> 'a) -> (unit -> 'b) -> (unit -> 'c) -> (unit -> 'd) -> (unit -> 'e) -> (unit -> 'f) -> (unit -> 'g) -> (unit -> 'h) -> 'a * 'b * 'c * 'd * 'e * 'f * 'g * 'h
-        val parallel9 : (unit -> 'a) -> (unit -> 'b) -> (unit -> 'c) -> (unit -> 'd) -> (unit -> 'e) -> (unit -> 'f) -> (unit -> 'g) -> (unit -> 'h) -> (unit -> 'i) -> 'a * 'b * 'c * 'd * 'e * 'f * 'g * 'h * 'i
+(* Knuth/Fisher-Yates shuffle *)
+(* Random is probably NOT thread safe, call in a locked context *)
+let shuffle l =
+    match l with [] | [_] -> l | _ ->
+    let a = Array.of_list l in
+    let rec loop i = if i <= 1 then () else 
+        let n = i - 1 in
+        let k = Random.int (n + 1) in
+        let t = Array.get a k in
+        Array.set a k (Array.get a n);
+        Array.set a n t;
+        loop n
+    in loop (Array.length a); Array.to_list a
 
-    end
+let with_mutex m f = (
+    Mutex.lock m; 
+    let v = try f m with e -> Mutex.unlock m; raise e in 
+    Mutex.unlock m;
+    v)
 
+let channel () = ref NobodyWaiting
 
-    module Channel : CHANNEL = struct
+let poison c = with_mutex global_mutex (fun _ -> 
+    let f = fun (s, _) -> Condition.signal s in
+    (match !c with
+    | ReaderWaiting xs -> List.iter f xs 
+    | WriterWaiting xs -> List.iter f xs
+    | _ -> ());
+    c := Poisoned)
 
-        type 'a slot = Empty | HasValue of 'a | Poison
+(* Must be called in a locked context *)
+let rec attempt_all l = match l with
+    | [] -> None
+    | (h::t) -> match h.attempt () with
+        | Some v -> Some v
+        | None -> attempt_all t
 
-        type ('a, 'c) t = 'a slot ref * Mutex.t * Condition.t * Condition.t * Condition.t
+(* Must be called in a locked context *)
+let rec check_poison_all l = match l with
+    | [] -> true
+    | (h::t) -> if h.check_poison () 
+        then check_poison_all t else false
 
-        exception PoisonException
+(* Must be called in a locked context *)
+let subscribe_all l r = let rec loop k = match k with
+    | [] -> ()
+    | (h::t) -> h.subscribe l r; loop t
+    in loop l
 
-        let channel () = (ref Empty, Mutex.create (), Condition.create (), Condition.create (), Condition.create ())
+(* Must be called in a locked context *)
+let rec unsubscribe_all l = match l with
+    | [] -> ()
+    | (h::t) -> (h.unsubscribe (); unsubscribe_all t)
+    
+(* Uses the global lock *)
+let select l = with_mutex global_mutex (fun m ->
+    let s = Condition.create () in
+    let l = List.map (fun x -> x s) (shuffle l) in
+    match attempt_all l with
+    | Some v -> v
+    | None -> let r = ref None in (subscribe_all l r;
+        let rec loop () = 
+            if check_poison_all l 
+            then (unsubscribe_all l; raise PoisonException) 
+            else match !r with
+            | Some v -> v
+            | None -> (Condition.wait s m; loop ())
+        in loop ()))
 
-        let with_mutex m f = (
-            Mutex.lock m; 
-            let v = try f m with e -> Mutex.unlock m; raise e in 
-            Mutex.unlock m;
-            v
-            )
+let transmit l r f v s =
+    r := Some (f v);
+    unsubscribe_all l;
+    Condition.signal s
 
-        let read (r, m, cr, cw, cd) = 
-            let rec aux m = (
-                match !r with
-                | Empty -> (
-                    Condition.wait cr m;
-                    aux m
-                    )
-                | HasValue v -> (
-                    r := Empty;
-                    Condition.signal cd;
-                    Condition.signal cw;
-                    v
-                    )
-                | Poison -> raise PoisonException
-                )
-            in with_mutex m aux
+(* Methods must be called in a locked context *)
+let read_guard c f s = {
+    attempt = (fun () -> match !c with
+        | WriterWaiting ((_, x)::_) -> Some (f (x ()))
+        | _ -> None
+    );
+    check_poison = (fun () -> !c == Poisoned);
+    subscribe = (fun l r ->
+        let g = (s, fun v -> transmit l r f v s) 
+        in match !c with
+        | NobodyWaiting -> c := ReaderWaiting [g]
+        | ReaderWaiting gs -> c := ReaderWaiting (gs @ [g])
+        | Poisoned -> () (* Ignore *)
+        | WriterWaiting _ -> () (* Shouldn't ever happen *)
+    );
+    unsubscribe = (fun () -> match !c with
+        | ReaderWaiting gs -> 
+            (match List.filter (fun (i, _) -> i != s) gs with
+            | [] -> c := NobodyWaiting
+            | gs -> c := ReaderWaiting gs)
+        | _ -> ()
+    );
+    }
 
-        let write (r, m, cr, cw, cd) v = 
-            let rec aux m = (
-                match !r with
-                | Empty -> (
-                    r := HasValue v;
-                    Condition.signal cr;
-                    Condition.wait cd m;
-                    (if !r = Poison then raise PoisonException);
-                    ()
-                    )
-                | HasValue _ -> (
-                    Condition.wait cw m;
-                    aux m
-                    )
-                | Poison -> raise PoisonException
-                )
-            in with_mutex m aux
+(* Methods must be called in a locked context *)
+let write_guard c v f s = {
+    attempt = (fun () -> match !c with
+        | ReaderWaiting ((_, x)::_) -> (x v; Some (f v))
+        | _ -> None
+    );
+    check_poison = (fun () -> !c == Poisoned);
+    subscribe = (fun l r -> 
+        let g = (s, fun () -> transmit l r f v s; v) 
+        in match !c with
+        | NobodyWaiting -> c := WriterWaiting [g]
+        | WriterWaiting gs -> c := WriterWaiting (gs @ [g])
+        | Poisoned -> () (* Ignore *)
+        | ReaderWaiting _ -> () (* Shouldn't ever happen *)
+    );
+    unsubscribe = (fun () -> match !c with
+        | WriterWaiting gs -> 
+            (match List.filter (fun (i, _) -> i != s) gs with
+            | [] -> c := NobodyWaiting
+            | gs -> c := WriterWaiting gs)
+        | _ -> ()
+    );
+    }
 
-        let poison (r, m, cr, cw, cd) = with_mutex m (fun m ->
-            r := Poison;
-            Condition.broadcast cr;
-            Condition.broadcast cw;
-            Condition.broadcast cd
-            )
-            
-        let poison_only c = c
-        let write_only c = c
-        let write_poison_only c = c
-        let read_only c = c
-        let read_poison_only c = c
-        let read_write_only c = c
+let read c = select [read_guard c (fun x -> x)]
+let write c v = select [write_guard c v (fun _ -> ())]
 
-    end
+let parallel fs = let fs = shuffle fs in
+    let ts = List.map (fun f -> Thread.create (fun () -> 
+        try f () with PoisonException -> ()) ()) fs
+    in List.iter Thread.join ts
 
+let parallel_collect fs f =
+    let r = ref (Left PoisonException) in
+    let f () = try r := Right (f ()) with e -> r := Left e in
+    parallel (f::fs);
+    match !r with
+    | Left e -> raise e
+    | Right v -> v
 
-    module Process : PROCESS = struct
-        
-        type 'a result = Good of 'a | Bad of exn
-        
-        exception NeverException
-
-        let parallel f1 f2 = 
-            let attempt f r = r := try Good (f ()) with e -> Bad e in
-            let r1 = ref (Bad NeverException) in 
-            let r2 = ref (Bad NeverException) in 
-            let t1 = Thread.create (fun () -> attempt f1 r1) () in (
-            attempt f2 r2;
-            Thread.join t1;
-            let extract r = match !r with
-                | Good v -> v
-                | Bad e -> raise e
-            in (extract r1, extract r2)
-            )
-            
-        let parallel3 f1 f2 f3 = let (v1, (v2, v3)) = parallel f1 (fun () -> parallel f2 f3) in (v1, v2, v3)
-        let parallel4 f1 f2 f3 f4 = let (v1, (v2, v3, v4)) = parallel f1 (fun () -> parallel3 f2 f3 f4) in (v1, v2, v3, v4)
-        let parallel5 f1 f2 f3 f4 f5 = let (v1, (v2, v3, v4, v5)) = parallel f1 (fun () -> parallel4 f2 f3 f4 f5) in (v1, v2, v3, v4, v5)
-        let parallel6 f1 f2 f3 f4 f5 f6 = let (v1, (v2, v3, v4, v5, v6)) = parallel f1 (fun () -> parallel5 f2 f3 f4 f5 f6) in (v1, v2, v3, v4, v5, v6)
-        let parallel7 f1 f2 f3 f4 f5 f6 f7 = let (v1, (v2, v3, v4, v5, v6, v7)) = parallel f1 (fun () -> parallel6 f2 f3 f4 f5 f6 f7) in (v1, v2, v3, v4, v5, v6, v7)
-        let parallel8 f1 f2 f3 f4 f5 f6 f7 f8 = let (v1, (v2, v3, v4, v5, v6, v7, v8)) = parallel f1 (fun () -> parallel7 f2 f3 f4 f5 f6 f7 f8) in (v1, v2, v3, v4, v5, v6, v7, v8)
-        let parallel9 f1 f2 f3 f4 f5 f6 f7 f8 f9 = let (v1, (v2, v3, v4, v5, v6, v7, v8, v9)) = parallel f1 (fun () -> parallel8 f2 f3 f4 f5 f6 f7 f8 f9) in (v1, v2, v3, v4, v5, v6, v7, v8, v9)
-     
-        let fork fs = let rec aux fs = match fs with
-            | f::fs -> ignore (parallel f (fun () -> aux fs))
-            | [] -> ()
-            in try aux fs with _ -> ()
-            
-        let generate_list n f = let rec aux i l = match i with
-            | 0 -> l
-            | i -> aux (i - 1) (f i :: l)
-            in aux n []
-            
-        let spawn n f = fork (generate_list n (fun i -> fun () -> f i))
-        
-    end
-
-
-    include Channel
-    include Process
-
-end
-
-(*
-    Proposal for alternation interface:
-    Csp.prioritized [
-        Csp.Read (c1, fun v -> v * v);
-        Csp.Read (c2, fun v -> v + v);
-        Csp.Write (c3, (fun () -> v), fun v -> 0);
-        Csp.Wait (0.100, fun t -> 0);
-    ]
-    Csp.arbitrary [ ... ] - may not be too interesting to support.
-    Csp.random [ ... ]
-    Csp.fair [ ... ] - can this be done with that interface?
-    In any case, random may be enough.
-*)
-
-(* (c1?x->skip||c2?x->skip); c3!(x+y) -> P ????? *)
+let read_only x = x
+let read_write_only x = x
+let read_poison_only x = x
+let write_only x = x
+let write_poison_only x = x
+let poison_only x = x
 

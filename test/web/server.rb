27ee36a398833dcebf2fafd4640eed51803d5043
gentauro <gentauro@gmail.com>
2009-07-11 23:38:41 +0200

    now the server don't crash on client stop. Handle mime types through /etc/mime.types

--- test/web/server.rb
+++ test/web/server.rb
@@ -1,20 +1,30 @@
 require 'socket'
 port = (ARGV[0] || 4040).to_i
-s =  (ARGV[1] || 3).to_i
+s =  (ARGV[1] || 0).to_i
 server = TCPServer.new('localhost', port)
 while (session = server.accept)
-  sleep(s)
   g = session.gets
   f = g.split(" ")[1]
   p = "mp3" + f
-  x = File.exist?(p)
-  if x
-    session.print "HTTP/1.1 200/OK\r\nServer: OcamlCSP\r\nContent-type: application/mp3\r\n\r\n"
+  if File.exist?(p)
+    e = File.extname(f)
+    c = "application/octet-stream"
+    File.open("/etc/mime.types").each { |line|
+      a = line.split(" ")
+      a.each { |ae| c = a[0] if e == ("."+ ae)}
+    }
+    session.print "HTTP/1.0 200/OK\r\nServer: " +
+      "OcamlCSP\r\nContent-type: #{c}\r\n\r\n"
     io = File.open(p, "rb")
     while (not io.eof?)
       sleep(s/1000.0)
       buffer = io.read(256)
-      session.write(buffer)
+      begin
+        session.write(buffer)
+      rescue Exception=>e
+        #if we can write we break out
+        break
+      end
     end
     io.close
   end

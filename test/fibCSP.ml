c2e67678b81f465c5e3b1ec8ce01bda95f02a58e
gentauro <gentauro@gmail.com>
2009-07-14 15:13:12 +0200

    poison don't seem to work correctly with fibonacciCSP ...

--- test/fibCSP.ml
+++ test/fibCSP.ml
@@ -1,18 +1,23 @@
 (* Fibonacci CSP network *)
 
-open Csp
 open Legoland
 
-let rec stop n i o () =
-    match n with
-      | 0 -> Csp.poison i; Csp.poison o; exit 0
-      | _ -> Csp.write o (Csp.read i); stop (n-1) i o ()
+let bii x = Big_int.big_int_of_int x
+let sbi x = Big_int.string_of_big_int x
+let (++) x y = Big_int.add_big_int x y
 
+let rec stop n i o () =
+  match n with
+    | 0 -> Csp.poison i; Csp.poison o; (* exit 0 *)
+    | _ -> Csp.write o (Csp.read i); stop (n-1) i o ()
+        
 let _ = 
   let c1 = Csp.channel () in
   let c2 = Csp.channel () in
-    Csp.parallel [
-      fibonacciInt c1;
-      stop 42 c1 c2;
-      printer c2
-    ]
+      Csp.parallel [
+        (* numbersInt c1; *)
+        fibonacciInt c1;
+        (* (fun () -> for x = 1 to 44 do Csp.write c1 (bii x) done); *)
+        stop 42 c1 c2;
+        printer c2
+      ]

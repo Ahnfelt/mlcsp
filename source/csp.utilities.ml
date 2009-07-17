a6d78a6192bf87b0ec4ec3d84c127e9f0c07403b
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2009-07-17 19:46:15 +0200

    Just breaking stuff

---
+++ source/csp.utilities.ml
@@ -0,0 +1,63 @@
+module Csp = struct module Utilities = struct
+
+(* Quite broken still *)
+
+let poison_list l fn () = 
+  try fn () with Csp.PoisonException -> List.iter (fun f -> f ()) l
+
+let poison_channel c () =
+  Csp.poison c
+
+let raise_poison () =
+  raise Csp.PoisonException
+
+let pc = poison_channel
+
+let finally f g = try f () with e -> g(); raise e
+
+let write_file f i () =
+    let io = open_out_bin f in
+    try while true do
+        output_string io (Csp.read i);
+        flush io
+    done with e -> close_out io; raise e
+
+let read_file f o () =
+    let io = open_in_bin f in
+    let size = 512 in
+    let b = String.create size in
+    let rec read_chunk s = match s with
+        | 0 -> Csp.poison o; close_in io
+        | _ -> Csp.write o (String.sub b 0 s); read_chunk (input io b 0 size)
+    in read_chunk (input io b 0 size)
+
+let read_lines f o () =
+    let io = finally (fun () -> open_in_bin f) (fun () -> Csp.poison o) in
+    try finally 
+        (fun () -> while true do Csp.write o (input_line io) done) 
+        (fun () -> Csp.poison o; close_in io) 
+    with End_of_file -> ()
+
+let delta c c1 c2 () = 
+    let pl = poison_list [pc c; pc c1; pc c2] in
+    try while true do
+        let v = Csp.read c in
+        Csp.parallel [
+            pl (fun () -> Csp.write c1 v);
+            pl (fun () -> Csp.write c2 v);
+        ]
+    done with e -> pl raise_poison (); raise e
+
+let printer c () =
+    try while true do print_endline (Csp.read c) done
+    with Csp.PoisonException -> Csp.poison c
+
+let _ =
+    let c = Csp.channel () in
+    Csp.parallel [
+        read_lines "test.txt" c;
+        printer c;
+    ]
+
+end end
+
---
+++ test/nonblocking.ml
@@ -0,0 +1,23 @@
+(* ocamlc -vmthread threads.cma unix.cma nonblocking.ml -o nonblocking && ./nonblocking *)
+
+open Unix
+
+(* Waits for user input *)
+let a () =
+    let i = in_channel_of_descr stdin in
+    print_endline "read from stdin";
+    print_endline (input_line i)
+
+(* Reads while waiting for user input *)
+let b () = 
+    let i = open_in "nonblocking.ml" in
+    Thread.delay 1.0;
+    print_endline "read from file";
+    print_endline (input_line i)
+
+(* Run the two threads in parallel *)
+let _ = 
+    let t1 = Thread.create a () in
+    let t2 = Thread.create b () in
+    Thread.join t1; Thread.join t2
+

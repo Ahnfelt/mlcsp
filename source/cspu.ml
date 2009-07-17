a671e4aec86e02ef0e27bca5df1974859fef499f
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2009-07-17 22:40:25 +0200

    Everything works. On my computers :P

--- source/cspu.ml
+++ source/cspu.ml
@@ -11,30 +11,24 @@ let raise_poison () =
 
 let pc = poison_channel
 
-let finally f g = try f () with e -> g(); raise e
+let finally f g = let v = try f () with e -> g (); raise e in g (); v
 
 let write_file f i () =
-    let io = open_out_bin f in
-    try while true do
+    let io = try open_out_bin f with e -> Csp.poison i; raise e in
+    finally (fun () -> while true do
         output_string io (Csp.read i);
         flush io
-    done with e -> close_out io; raise e
+    done) (fun () -> Csp.poison i; close_out io)
 
 let read_file f o () =
-    let io = open_in_bin f in
+    let io = try open_in_bin f with e -> Csp.poison o; raise e in
     let size = 512 in
     let b = String.create size in
-    let rec read_chunk s = match s with
-        | 0 -> Csp.poison o; close_in io
+    finally (fun () ->
+        let rec read_chunk s = match s with
+        | 0 -> ()
         | _ -> Csp.write o (String.sub b 0 s); read_chunk (input io b 0 size)
-    in read_chunk (input io b 0 size)
-
-let read_lines f o () =
-    let io = finally (fun () -> open_in_bin f) (fun () -> Csp.poison o) in
-    try finally 
-        (fun () -> while true do Csp.write o (input_line io) done) 
-        (fun () -> Csp.poison o; close_in io) 
-    with End_of_file -> ()
+    in read_chunk (input io b 0 size)) (fun () -> Csp.poison o; close_in io)
 
 let delta c c1 c2 () = 
     let pl = poison_list [pc c; pc c1; pc c2] in
@@ -49,11 +43,4 @@ let delta c c1 c2 () =
 let printer c () =
     try while true do print_endline (Csp.read c) done
     with Csp.PoisonException -> Csp.poison c
-
-let _ =
-    let c = Csp.channel () in
-    Csp.parallel [
-        read_lines "test.txt" c;
-        printer c;
-    ]
 
--- test/buildSockets.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-clear
-ocamlc -I '../source/' -vmthread threads.cma -c ../source/csp.mli ../source/csp.ml
-ocamlc -I '../source/' -vmthread threads.cma -c sockets.ml
-ocamlc -I '../source/' -vmthread threads.cma unix.cma str.cma -o sockets csp.cmo sockets.cmo

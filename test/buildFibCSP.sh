e9377b3124a98498415ddc973c37a26c28652c9c
gentauro <gentauro@gmail.com>
2009-07-19 14:38:36 +0200

    todo file for report

--- test/buildFibCSP.sh
+++ test/buildFibCSP.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 clear
 ocamlc -I '../source/' -vmthread threads.cma -c ../source/csp.mli ../source/csp.ml
+ocamlc -I '../source/' -c ../source/cspu.ml
 ocamlc -I '../source/' -c ../source/legoland.ml
 ocamlc -I '../source/' -c fibCSP.ml
-ocamlc -I '../source/' -vmthread threads.cma nums.cma -o fibCSP csp.cmo legoland.cmo fibCSP.cmo
+ocamlc -I '../source/' -vmthread threads.cma nums.cma -o fibCSP csp.cmo cspu.cmo legoland.cmo fibCSP.cmo
--- test/fibCSP.ml
+++ test/fibCSP.ml
@@ -1,11 +1,7 @@
 (* Fibonacci CSP network *)
 
 open Legoland
-
-let bii x = Big_int.big_int_of_int x
-let sbi x = Big_int.string_of_big_int x
-let (++) x y = Big_int.add_big_int x y
-       
+      
 let _ = 
   let c1 = Csp.channel () in
   let c2 = Csp.channel () in

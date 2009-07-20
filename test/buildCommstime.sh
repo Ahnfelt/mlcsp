529430825d50be65dad9de63d8398aa13936df3c
gentauro <gentauro@gmail.com>
2009-07-20 18:07:49 +0200

    wrote some report and made commstime test ala python ...

--- test/buildCommstime.sh
+++ test/buildCommstime.sh
@@ -3,5 +3,5 @@ clear
 ocamlc -I '../source/' -vmthread threads.cma -c ../source/csp.mli ../source/csp.ml
 ocamlc -I '../source/' -c ../source/cspu.ml
 ocamlc -I '../source/' -c ../source/legoland.ml
-ocamlc -I '../source/' -c ssn.ml
-ocamlc -I '../source/' -vmthread threads.cma nums.cma -o ssn csp.cmo cspu.cmo legoland.cmo ssn.cmo
+ocamlc -I '../source/' -c commstime.ml
+ocamlc -I '../source/' -vmthread unix.cma threads.cma nums.cma -o commstime csp.cmo cspu.cmo legoland.cmo commstime.cmo

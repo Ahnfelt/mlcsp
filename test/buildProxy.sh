fb23c92dd1139d55d13a958004e0742f04515ed5
gentauro <gentauro@gmail.com>
2009-07-28 22:04:16 +0200

    ...

--- test/buildProxy.sh
+++ test/buildProxy.sh
@@ -3,4 +3,4 @@ clear
 ocamlc -I '../source/' -vmthread threads.cma -c ../source/csp.mli ../source/csp.ml
 ocamlc -I '../source/' -vmthread threads.cma -c ../source/cspu.ml
 ocamlc -I '../source/' -vmthread threads.cma -c proxyCSP.ml
-ocamlc -I '../source/' -vmthread threads.cma unix.cma str.cma -o proxyCSP csp.cmo cspu.cmo proxyCSP.cmo
+ocamlc -I '../source/' -vmthread unix.cma threads.cma str.cma -o proxyCSP csp.cmo cspu.cmo proxyCSP.cmo
